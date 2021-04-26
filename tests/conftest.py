b5df207605578f1009afb439a5c76ce80800155f
atcswap <78479480+atcswap@users.noreply.github.com>
2021-04-26 14:05:44 +0800

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+import atc_chain.utils.bls as bls
+from atc_chain.utils.blake import blake
+
+
+@pytest.fixture(scope="session")
+def privkeys():
+    return [int.from_bytes(blake(str(i).encode('utf-8'))[:4], 'big') for i in range(1000)]
+
+
+@pytest.fixture(scope="session")
+def keymap(privkeys):
+    keymap = {}
+    for i, k in enumerate(privkeys):
+        keymap[bls.privtopub(k)] = k
+        if i % 50 == 0:
+            print("Generated %d keys" % i)
+    return keymap
+
+
+@pytest.fixture(scope="session")
+def pubkeys(keymap):
+    return list(keymap)
